71492e0874c523b4a4e6595fbf7bacd6f719555f
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2022-12-23 23:39:51 +0000

    Add files via upload
    changed the insert function so that you can move to the next, previous node easily

--- double_linkedlist.py
+++ double_linkedlist.py
@@ -7,32 +7,37 @@ class DoubleLinkedlist:
     def __init__(self):
 
         self.head = None
-    def insert_right_end(self,node):
+        self.tail = None
+    def insert_right(self,node):
+        new_node = Node(node)
         if self.head == None:
-            new_node = Node(node)
             self.head = new_node
-            
-        
+            return
         cur_node = self.head
         while cur_node:
             if cur_node.next == None:
+                
                 new_node = Node(node)
+                prev_node = new_node
+                prev_node.prev = cur_node
                 cur_node.next = new_node
-                cur_node.prev = cur_node
-                print(cur_node.next.data)
-                print(cur_node.prev.data)
+    
+    
                 break
             cur_node = cur_node.next
+    def insert_left(self,node):
+        pass
+        
 
 
 l = DoubleLinkedlist()
-l.insert_right_end(10)
-l.insert_right_end(20)
-l.insert_right_end(30)
-l.insert_right_end(40)
+l.insert_right(10)
+l.insert_right(20)
+l.insert_right(30)
+l.insert_right(40)
 print()
-print(l.head.next.next.data)
-print(l.head.next.next.prev.data)
+print(l.head.next.next.prev.prev.data)
+
 
             
         
