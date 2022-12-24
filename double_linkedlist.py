07e6f3fb263654adfd1faf8d2552d104e5693adb
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2022-12-24 19:14:38 +0000

    Add files via upload
    improved the the function to add a  node from the tail of the doubly linked list

--- double_linkedlist.py
+++ double_linkedlist.py
@@ -26,33 +26,56 @@ class DoubleLinkedlist:
     
                 break
             cur_node = cur_node.next
-    def insert_left(self,node):
+    def insert_left(self,before, node):
           new_node= Node(node)
           if self.tail == None:
               self.tail = new_node
           cur_node = self.tail
         
           while cur_node:
-            if cur_node.prev != None:
+            if cur_node.data == before:
+                side_left = cur_node.prev
+                side_right = cur_node
+                
                 
                 new_node = Node(node)
                 prev_node = new_node
-                prev_node.next = cur_node
-                cur_node.next = prev_node
+                prev_node.next = side_right
+                side_right.prev = prev_node
+                prev_node.prev = side_left
+                side_left.next = prev_node
+            
                 break
-            cur_node.next = cur_node.prev
+                
+            cur_node = cur_node.prev
+
+    def print_nodes(self):
+        if self.head == None:
+            return
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+        
            
               
         
 
 l = DoubleLinkedlist()
 l.insert_right(10)
+l.insert_right(40)
 l.insert_right(20)
 l.insert_right(30)
-l.insert_right(40)
-
+l.print_nodes()
 print()
-print(l.tail.prev.prev.prev.data)
+l.insert_left(20,7)
+l.insert_left(30,8)
+l.insert_left(40,5)
+l.insert_left(7,100)
+print()
+print()
+
+l.print_nodes()
 
 
             
