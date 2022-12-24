87461e42b5a76af22b3028af7262cf90ce628228
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2022-12-24 01:01:08 +0000

    Add files via upload
    Added a  function to add a node to a  right end and left end

--- double_linkedlist.py
+++ double_linkedlist.py
@@ -21,22 +21,38 @@ class DoubleLinkedlist:
                 prev_node = new_node
                 prev_node.prev = cur_node
                 cur_node.next = new_node
+                self.tail= cur_node.next
     
     
                 break
             cur_node = cur_node.next
     def insert_left(self,node):
-        pass
+          new_node= Node(node)
+          if self.tail == None:
+              self.tail = new_node
+          cur_node = self.tail
         
-
+          while cur_node:
+            if cur_node.prev != None:
+                
+                new_node = Node(node)
+                prev_node = new_node
+                prev_node.next = cur_node
+                cur_node.next = prev_node
+                break
+            cur_node.next = cur_node.prev
+           
+              
+        
 
 l = DoubleLinkedlist()
 l.insert_right(10)
 l.insert_right(20)
 l.insert_right(30)
 l.insert_right(40)
+
 print()
-print(l.head.next.next.prev.prev.data)
+print(l.tail.prev.prev.prev.data)
 
 
             
